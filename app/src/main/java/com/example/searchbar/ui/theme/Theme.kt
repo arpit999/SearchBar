6ea5123856d2c2bfbf32a81eda05098fab5404d6
arpit <arpitpatel009@gmail.com>
2024-03-23 16:06:41 -0400

    Added theme for colourful searchBar

--- app/src/main/java/com/example/searchbar/ui/theme/Theme.kt
+++ app/src/main/java/com/example/searchbar/ui/theme/Theme.kt
@@ -1,6 +1,21 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.searchbar.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,62 +24,86 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+// Material 3 color schemes
+private val searchBarDarkColorScheme = darkColorScheme(
+    primary = searchBarDarkPrimary,
+    onPrimary = searchBarDarkOnPrimary,
+    primaryContainer = searchBarDarkPrimaryContainer,
+    onPrimaryContainer = searchBarDarkOnPrimaryContainer,
+    inversePrimary = searchBarDarkPrimaryInverse,
+    secondary = searchBarDarkSecondary,
+    onSecondary = searchBarDarkOnSecondary,
+    secondaryContainer = searchBarDarkSecondaryContainer,
+    onSecondaryContainer = searchBarDarkOnSecondaryContainer,
+    tertiary = searchBarDarkTertiary,
+    onTertiary = searchBarDarkOnTertiary,
+    tertiaryContainer = searchBarDarkTertiaryContainer,
+    onTertiaryContainer = searchBarDarkOnTertiaryContainer,
+    error = searchBarDarkError,
+    onError = searchBarDarkOnError,
+    errorContainer = searchBarDarkErrorContainer,
+    onErrorContainer = searchBarDarkOnErrorContainer,
+    background = searchBarDarkBackground,
+    onBackground = searchBarDarkOnBackground,
+    surface = searchBarDarkSurface,
+    onSurface = searchBarDarkOnSurface,
+    inverseSurface = searchBarDarkInverseSurface,
+    inverseOnSurface = searchBarDarkInverseOnSurface,
+    surfaceVariant = searchBarDarkSurfaceVariant,
+    onSurfaceVariant = searchBarDarkOnSurfaceVariant,
+    outline = searchBarDarkOutline
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val searchBarLightColorScheme = lightColorScheme(
+    primary = searchBarLightPrimary,
+    onPrimary = searchBarLightOnPrimary,
+    primaryContainer = searchBarLightPrimaryContainer,
+    onPrimaryContainer = searchBarLightOnPrimaryContainer,
+    inversePrimary = searchBarLightPrimaryInverse,
+    secondary = searchBarLightSecondary,
+    onSecondary = searchBarLightOnSecondary,
+    secondaryContainer = searchBarLightSecondaryContainer,
+    onSecondaryContainer = searchBarLightOnSecondaryContainer,
+    tertiary = searchBarLightTertiary,
+    onTertiary = searchBarLightOnTertiary,
+    tertiaryContainer = searchBarLightTertiaryContainer,
+    onTertiaryContainer = searchBarLightOnTertiaryContainer,
+    error = searchBarLightError,
+    onError = searchBarLightOnError,
+    errorContainer = searchBarLightErrorContainer,
+    onErrorContainer = searchBarLightOnErrorContainer,
+    background = searchBarLightBackground,
+    onBackground = searchBarLightOnBackground,
+    surface = searchBarLightSurface,
+    onSurface = searchBarLightOnSurface,
+    inverseSurface = searchBarLightInverseSurface,
+    inverseOnSurface = searchBarLightInverseOnSurface,
+    surfaceVariant = searchBarLightSurfaceVariant,
+    onSurfaceVariant = searchBarLightOnSurfaceVariant,
+    outline = searchBarLightOutline
 )
 
 @Composable
 fun SearchBarTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
+    val searchBarColorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
+        darkTheme -> searchBarDarkColorScheme
+        else -> searchBarLightColorScheme
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
+        colorScheme = searchBarColorScheme,
+        typography = searchBarTypography,
+        shapes = shapes,
         content = content
     )
 }
